660c53c8c4484414f84c4cb64a74a7dac6d5a39f
Wade Peterson, Komran Rashidov and Kiran Surdhar <wpeterson@rallydev.com>
2014-10-06 11:09:52 -0600

    Treeact (part 1)

---
+++ src/apps/treeact/TreeactApp.js
@@ -0,0 +1,17 @@
+(function() {
+    var Ext = window.Ext4 || window.Ext;
+
+    Ext.define('Rally.apps.treeact.TreeactApp', {
+        extend: 'Rally.app.App',
+        cls: 'treeact-app',
+        alias: 'widget.treeactapp',
+        appName: 'Treeact',
+
+        launch: function() {
+            debugger
+//            this.add({
+//                html: 'HELLO, THIS IS TREEACT.'
+//            });
+        }
+    });
+})();
