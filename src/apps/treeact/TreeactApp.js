2bf941a6c22ea4911038932d1e1f4cea2be62311
Wade Peterson <wpeterson@rallydev.com>
2014-10-06 14:52:24 -0600

    Talk to react app

--- src/apps/treeact/TreeactApp.js
+++ src/apps/treeact/TreeactApp.js
@@ -9,7 +9,23 @@
 
         launch: function() {
             this.add({
-                html: 'HELLO, THIS IS TREEACT.'
+                itemId: 'treeact-container',
+                xtype: 'component',
+                html: '<div class="treeact-container"></div>'
+            });
+
+            if (this.rendered) {
+                this._init();
+            } else {
+                this.on('afterrender', this._init, this, { single: true });
+            }
+        },
+
+        _init: function () {
+            var container = this.down('#treeact-container').getEl().dom;
+
+            $.getScript('http://localhost:3000/bundle.js').then(function () {
+                treeact.ui.render(container);
             });
         }
     });
